1fee8ca20632e4135b8576b1e6593cb990ab5265
Anubhav Choudhary <deprov447@Anubhavs-MacBook-Air.local>
2025-05-29 11:20:48 +0530

    Fails over API key incase of quotaExpired

--- src/ingestion/fetchData.js
+++ src/ingestion/fetchData.js
@@ -1,57 +1,82 @@
 const axios = require("axios");
 const Video = require("../models/Video");
+let { apikey, failoverApiKey } = require("./apiKeyFailover");
 
 async function callYTAPI(publishedAfter, pageToken) {
-  let config = {
-    method: "GET",
-    url: "https://youtube.googleapis.com/youtube/v3/search",
-    headers: {
-      Accept: "application/json",
-    },
-    params: {
-      q: "official",
-      part: "snippet",
-      maxResults: process.env.MAX_RESULTS_PER_PAGE || 5,
-      order: "date",
-      type: "video",
-      key: process.env.API_KEY,
-      publishedAfter,
-      pageToken,
-    },
-  };
+  try {
+    let config = {
+      method: "GET",
+      url: "https://youtube.googleapis.com/youtube/v3/search",
+      headers: {
+        Accept: "application/json",
+      },
+      params: {
+        q: "official",
+        part: "snippet",
+        maxResults: process.env.MAX_RESULTS_PER_PAGE || 5,
+        order: "date",
+        type: "video",
+        key: apikey,
+        publishedAfter,
+        pageToken,
+      },
+    };
 
-  if (pageToken == null) delete config.params.pageToken;
+    if (pageToken == null) delete config.params.pageToken;
 
-  return await axios
-    .request(config)
-    .then((response) => {
-      return response.data;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    return await axios
+      .request(config)
+      .then((response) => {
+        return response.data;
+      })
+      .catch(async (error) => {
+        if (
+          error.status == 403 &&
+          error.response.data.error.errors[0].reason == "quotaExceeded"
+        ) {
+          console.log("QuotaExceeded");
+          apikey = failoverApiKey();
+          return await callYTAPI(publishedAfter, pageToken);
+        } else throw "Request failed with error: " + error;
+      });
+  } catch (err) {
+    throw err;
+  }
 }
 
 async function fetchData(publishedAfter) {
-    console.log('Fetching YT data published after: '+publishedAfter)
-    let parsedYTRes = []
-    let nextPageToken = null
-    let pagesTraversed = 0
+  try {
+    console.log("Fetching YT data published after: " + publishedAfter);
+    let parsedYTRes = [];
+    let nextPageToken = null;
+    let pagesTraversed = 0;
     do {
-        const ytRes = await callYTAPI(publishedAfter, nextPageToken)
+      try {
+        const ytRes = await callYTAPI(publishedAfter, nextPageToken);
         pagesTraversed++;
-        parsedYTRes = ytRes.items.map(({id, snippet})=>{
-            return {
-                videoId: id.videoId,
-                title: snippet.title,
-                description: snippet.description,
-                publishedAt: snippet.publishedAt,
-                thumbnails: snippet.thumbnails
-            }
-        })
-        nextPageToken = ytRes.nextPageToken || null
-    } while (nextPageToken && pagesTraversed < process.env.MAX_PAGES_TO_TRAVERSE);
-    await Video.insertMany(parsedYTRes, {ordered: false})
+        parsedYTRes = ytRes.items.map(({ id, snippet }) => {
+          return {
+            _id: id.videoId,
+            title: snippet.title,
+            description: snippet.description,
+            publishedAt: snippet.publishedAt,
+            thumbnails: snippet.thumbnails,
+          };
+        });
+        nextPageToken = ytRes.nextPageToken || null;
+      } catch (err) {
+        console.log(err);
+      }
+    } while (
+      nextPageToken &&
+      pagesTraversed < process.env.MAX_PAGES_TO_TRAVERSE
+    );
+    await Video.insertMany(parsedYTRes, { ordered: false }).catch((err) => {
+      if (err.code !== 11000) console.error(err);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = fetchData;
