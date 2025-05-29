85c99190e3ef5d812a785ac636161d882fa9b711
Anubhav Choudhary <deprov447@Anubhavs-MacBook-Air.local>
2025-05-29 11:00:37 +0530

    Implements logic for API key failover

---
+++ src/ingestion/apiKeyFailover.js
@@ -0,0 +1,24 @@
+let { apikey } = require("./apiKeyFailover");
+
+function failoverApiKey() {
+  const currentKey = apikey;
+  const allkeys = Object.entries(process.env).filter((kv) =>
+    kv[0].startsWith("API_KEY_")
+  );
+  let indexOfCurrentKey = 0;
+  for (let i = 0; i < allkeys.length; i++) {
+    if (allkeys[i][1] == currentKey) {
+      indexOfCurrentKey = i;
+      break;
+    }
+  }
+  if (indexOfCurrentKey == allkeys.length - 1) {
+    console.log("Quota for all keys expired");
+  } else {
+    console.log('moving to next api key')
+    apikey = allkeys[indexOfCurrentKey+1][1]
+  }
+  return apikey
+}
+
+module.exports = { failoverApiKey, apikey: process.env.API_KEY_1 };
